2fc150e236729dbda8ac71b5baddf378ea4afb4f
yondav <yoni@yondav.us>
2022-05-11 00:06:02 -0400

    cleanup and main body transitions

--- src/components/entry/index.tsx
+++ src/components/entry/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import { RichText } from '@graphcms/rich-text-react-renderer';
+import { fadeBounceDown, transitions } from '../../styles/framerVariants';
 
 const Entry = (props: { entry }) => (
   <>
     {props.entry && (
-      <article tw='mx-auto my-8 w-3/4 p-4 bg-neutral-200 rounded-lg'>
+      <motion.article
+        variants={fadeBounceDown}
+        initial='hidden'
+        whileInView='visible'
+        transition={transitions.smooth}
+        tw='mx-auto w-3/4 p-4 bg-neutral-200 rounded-lg'
+      >
         {props.entry.__typename.toLowerCase() === 'page' && (
           <h1 className='grdTxt' tw='mb-5'>
             {props.entry?.title}
@@ -21,7 +29,7 @@ const Entry = (props: { entry }) => (
           </h3>
         )}
         {props.entry?.description.raw && <RichText content={props.entry?.description?.raw} />}
-      </article>
+      </motion.article>
     )}
   </>
 );
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,9 +1,8 @@
 import React, { useContext, useRef, useState, useEffect } from 'react';
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { Outlet, useParams, Link } from 'react-router-dom';
 
 import { ThemeContext } from '../../context/theme';
-import { StoreContext } from '../../context/store/store.context';
 import useMediaQuery from '../../hooks/useMediaQuery';
 
 import Greek, { paths } from '../../components/svg/icon.greek';
@@ -11,12 +10,27 @@ import Nav from '../../components/nav';
 import SideBar from '../../components/sidebar';
 import Footer from '../../components/footer';
 
+const BreadCrumb = (props: { endpoint: string; slug: string; bullet?: boolean }) => {
+  const deformSlug = slug => {
+    return slug
+      .split('-')
+      .map(word => word[0].toUpperCase() + word.substr(1).toLowerCase())
+      .join(' ');
+  };
+
+  return (
+    <motion.span initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.1 }}>
+      <Link to={props.endpoint}>{deformSlug(props.slug)}</Link>
+      {props.bullet && <span tw='mx-1'>&bull;</span>}
+    </motion.span>
+  );
+};
+
 const Main = () => {
   const footerRef = useRef<HTMLElement | null>(null);
   const navRef = useRef<HTMLElement | null>(null);
   const [mainHeight, setMainHeight] = useState<string>('');
   const { sideBar } = useContext(ThemeContext);
-  const { loading, error } = useContext(StoreContext);
   const { page, section, subsection } = useParams();
   const { isDesktop, isTablet, isMobile, updateMedia } = useMediaQuery();
 
@@ -29,52 +43,34 @@ const Main = () => {
   useEffect(() => {
     const unsubscribe = () => {
       if (footerRef.current && navRef.current)
-        // @ts-ignore
         return setMainHeight(`calc(100vh - ${footerRef?.current?.offsetHeight + navRef?.current?.offsetHeight}px)`);
     };
     unsubscribe();
   }, [footerRef, navRef]);
 
-  const deformSlug = slug => {
-    return slug
-      .split('-')
-      .map(word => word[0].toUpperCase() + word.substr(1).toLowerCase())
-      .join(' ');
-  };
-
   return (
-    <div tw='min-h-screen w-screen flex flex-col justify-between transition-all duration-300 ease-in-out flex flex-col'>
+    <>
       <Nav ref={navRef} />
       <main tw='w-full flex relative' style={{ height: mainHeight }}>
         <AnimatePresence>
           {sideBar && <SideBar height={`calc(100vh - ${navRef?.current?.offsetHeight}px)`} />}
         </AnimatePresence>
-        <section tw='w-full px-3'>
+        <section tw='w-full px-3 mt-3'>
           <span tw='text-primary text-xs'>
-            {page && (
-              <>
-                <Link to={`/${page}`}>{deformSlug(page)}</Link>
-                <span tw='mx-1'>&bull;</span>
-              </>
-            )}
-            {section && (
-              <>
-                <Link to={`/${page}/${section}`}>{deformSlug(section)}</Link>
-                <span tw='mx-1'>&bull;</span>
-              </>
-            )}
-            {subsection && <Link to={`/${page}/${section}/${subsection}`}>{deformSlug(subsection)}</Link>}
+            {page && <BreadCrumb endpoint={`/${page}`} slug={page} bullet={!!section} />}
+            {section && <BreadCrumb endpoint={`/${page}/${section}`} slug={section} bullet={!!subsection} />}
+            {subsection && <BreadCrumb endpoint={`/${page}/${section}/${subsection}`} slug={subsection} />}
           </span>
-          <section tw='overflow-y-scroll max-h-[95%]'>
+          <div tw='overflow-y-scroll max-h-[95%] my-4'>
             {paths({ isDesktop, isTablet, isMobile }).map((path, i) => (
               <Greek key={i} d={path.d} fill={path.fill} style={path.style} />
             ))}
             <Outlet />
-          </section>
+          </div>
         </section>
       </main>
       <Footer ref={footerRef} />
-    </div>
+    </>
   );
 };
 
